ca76dfba71d9d6e63ca495cdadcf814fc8a0209e
Rafael Castro <rcastr231@gmail.com>
2025-12-22 05:09:38 -0800

    Token Model Class Update

--- Models/WorkToken.cs
+++ Models/WorkToken.cs
@@ -1,6 +1,27 @@
-﻿namespace TokenFlow_API_MS.Models
+﻿using System;
+
+namespace TokenFlow_API_MS.Models
 {
     public class WorkToken
     {
+        public Guid tokenID { get; }
+        public DateTime createdAt { get; }
+        public string? tokenName { get; private set; }
+        public string tokenType { get; private set; }
+        public string? createdBy { get; private set; }
+        public string Priority { get; private set; } = "Meduim";
+
+      // constructor to set token details automatically when created 
+
+        public WorkToken(string tokenName)
+        {
+            this.tokenID = Guid.NewGuid(); // here Guid is used to generate a unique ID
+            this.createdAt = DateTime.UtcNow; // timestamp for token creation
+            this.tokenName = tokenName; // name of the token (ex. = " Finish New Hire Onboarding Feb")
+
+        }
+
     }
 }
+
+
