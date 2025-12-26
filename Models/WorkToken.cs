4e589be74d68ebf57f3b1c176432e855e4449cf8
Rafael Castro <rcastr231@gmail.com>
2025-12-25 22:56:20 -0800

    fix for WorkToken.cs

--- Models/WorkToken.cs
+++ Models/WorkToken.cs
@@ -1,27 +1,37 @@
 ﻿using System;
 
 
+
+//TO DEVELOPERS: A TOKEN IN THIS CASE IS A VIRTUAL WORK TICKET WHICH CONTAINS A GUID, TICKTE INFORMATION, DUE DATE, ETC. // 
+
+
 namespace TokenFlow_API_MS.Models
 {
-    public class WorkToken  // this class represents what each and every work/task token will possess.
+    public class WorkToken  // this class represents what a token will contain, class members below
     {
-        public Guid tokenID { get; }  // guid is part of the overall system for randomized id generation. 
-        public DateTime createdAt { get; }
-        public string? tokenName { get; private set; }
-        public string? tokenType { get; private set; }
-        public string? tokenInfo { get; private set; }
-        public string? createdBy { get; private set; }
-        public string Priority { get; private set; } = "Medium";
+
+        // CLASS MEMBERS INSIDE WORKTOKEN CLASS // 
+
+        public Guid tokenID { get; }  // guid is part of the overall system for randomized id generation. // FIELD
+        public DateTime createdAt { get; } // FIELD
+        public string? tokenName { get; private set; } // FIELD
+        public string? tokenType { get; private set; } // FIELD
+        public string? tokenInfo { get; private set; } // FIELD 
+        public User? AssignedUser { get; private set; }
+        public TokenState currentState { get; private set; } = TokenState.Draft;
+
+        public string? createdBy { get; private set; } // FIELD
+        public string Priority { get; private set; } = "Medium";  //FIELD
         public DateTime dueDate { get; private set; }
 
         // public TokenState currentState { get; private set; } = TokenState.Draft;        
 
 
-      // constructor to set token details automatically when created 
+        // constructor to set token details automatically when created 
 
-        public WorkToken(string? tokenName, string? tokenInfo, string? tokenType, string? createdBy, DateTime? dueDate = null )  // data details set once token is created , note tokenID & createdAt are not present in the parameters as they are automatically crated by generation and by user.
+        public WorkToken(string? tokenName, string? tokenInfo, string? tokenType, string? createdBy, DateTime? dueDate = null)  // data details set once token is created , note tokenID & createdAt are not present in the parameters as they are automatically crated by generation and by user.
         {
-            if  ( tokenName == null )
+            if (tokenName == null)
                 tokenID = Guid.Empty;
 
 
@@ -37,7 +47,35 @@ namespace TokenFlow_API_MS.Models
 
         }
 
+        // method for assignToken
 
+        public void assignUser(User user)
+        {
+            if (user == null)
+            {
+                return;
+            }
+
+
+            if (AssignedUser != null)
+            {
+                return;
+            }
+
+            AssignedUser = user;
+
+        }
+        
+        
+
+
+        // method for updateState
+
+        public void updateState(TokenState newState)
+
+        {
+
+        }
 
 
     }
