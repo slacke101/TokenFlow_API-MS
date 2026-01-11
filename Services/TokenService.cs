aee9be97254cfa2231f72ef0e56298e22dcc4e3e
slacke101 <rcastr231@gmail.com>
2026-01-10 18:37:39 -0800

    updated worktoken, tokenstate, and tokenservice.cs pt1

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,9 +1,13 @@
-﻿
+﻿using TokenFlow_API_MS.Models;
+using System;
+using System.Collections;
+
 
 
 namespace TokenFlow_API_MS.Services
 {
     public class TokenService
     {
+        public token Work
     }
 }
