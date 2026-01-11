5b9ffd333f8ac13d4524edfac23798c8ce676e58
Rafael Castro <rcastr231@gmail.com>
2026-01-11 01:06:50 -0800

    tokencontroller.cs GET method for token by ID

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -1,6 +1,7 @@
 ﻿using TokenFlow_API_MS.Services;
 using Microsoft.AspNetCore.Mvc;
 using TokenFlow_API_MS.Models;
+using System.Reflection.Metadata.Ecma335;
 
 namespace TokenFlow_API_MS.Controllers
 {
@@ -10,10 +11,24 @@ namespace TokenFlow_API_MS.Controllers
     public class TokenController : ControllerBase
     {
         // GET method for token retrieval 
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult GetAll()
+        {
+            var tokens = _TokenService.GetAll();
+            return Ok(tokens);
+        }
 
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var token = _tokenService.Get(id);
 
+            if (token == null)
+                return NotFound("Token not found");
 
-
+            return Ok(token);
+        }
 
 
 
